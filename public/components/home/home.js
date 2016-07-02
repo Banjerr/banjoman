fccaa74d7265c73f5b9468aed55bf073996984c8
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-07-02 06:29:38 -0600

    hell yeah\!, can change chords

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -5,10 +5,13 @@ angular.module('banjoman')
     templateUrl: '/components/home/home.html',
     controller: function ($scope,$log,$timeout,ngAudio,stringService) {
 
-      var synth = new Tone.FMSynth().toMaster();
+      $scope.chord = "G";
+      $scope.tester = 'jhi'
+
+
       let s = stringService;
       let play = ngAudio.play;
-      $scope.tunings = {
+      let obj = {
         Gmajor:
         {
           G:
@@ -23,128 +26,27 @@ angular.module('banjoman')
           {
             one: s.E4,
             two: s.Db4,
-            three:s.A4,
+            three:s.A3,
             four:s.E3,
             five:s.A4
           }
         }
       }
 
-      class Player {
-        constructor(tuning) {
-          this.one = tuning.one;
-          this.two = tuning.two;
-          this.three = tuning.three;
-          this.four = tuning.four;
-          this.five = tuning.five;
+      // class Player {
+      //   constructor(tuning) {
+      //     this.one = tuning.one;
+      //     this.two = tuning.two;
+      //     this.three = tuning.three;
+      //     this.four = tuning.four;
+      //     this.five = tuning.five;
+      //   }
+       $scope.tuning="Gmajor";
+       $scope.chord="G";
+        $scope.play = function(tuning,chord,string){
+          return ngAudio.play(obj[tuning][chord][string])
         }
-        play1(){
-          $log.info('triggered');
-          return ngAudio.play(this.one)
-        }
-        play2(){
-          $log.info('triggered');
-          return ngAudio.play(this.two)
-        }
-        play3(){
-          $log.info('triggered');
-          return ngAudio.play(this.three)
-        }
-        play4(){
-          $log.info('triggered');
-          return ngAudio.play(this.four)
-        }
-        play5(){
-          $log.info('triggered');
-          return ngAudio.play(this.five)
-        }
-      }
-      $scope.A = new Player($scope.tunings.Gmajor.G)
-      $log.info('scopeA:',$scope.A)
-      // class Tuning extends Object {
-      //     constructor(){
-
+      
     }
-    // setStrings(tuning){
-    //   return
-    //   this.one = tuning.one;
-    //   this.two = tuning.two;
-    //   this.three = tuning.three;
-    //   this.four = tuning.four;
-    //   this.five = tuning.five;
-    // }
-    //   playSound1(tuning){
-    //     return
-    //     ngAudio.play(tuning.one);
-    //   }
-    //   playSound2(tuning){
-    //     return
-    //     ngAudio.play(tuning.two);
-    //   }
-    //   playSound3(tuning){
-    //     return
-    //     ngAudio.play(tuning.three);
-    //   }
-    //   playSound4(tuning){
-    //     return
-    //     ngAudio.play(tuning.four);
-    //   }
-    //   playSound5(tuning){
-    //     return
-    //     ngAudio.play(tuning.five);
-    //   }
-    // }
-    // this.Tuning.set(Strings)
-    // $scope.Tuning.setStrings($scope.tunings.Gmajor.A)
-
-    // $log.info('tuning:',tuning)
-    // $log.info("Gmaj:",$scope.tunings.Gmajor.A.one)
-    // $scope.setStrings = function(tuning){
-    //   return
-    //   $scope.playSynth1(tuning.one);
-    //   $scope.playSynth2(tuning.two);
-    //   $scope.playSynth3(tuning.three);
-    //   $scope.playSynth4(tuning.four);
-    //   $scope.playSynth5(tuning.five);
   }
-  // $log.info('PS1',$scope.playSynth1)
-  // $scope.playSynth1 = function(note){
-  //   return  ngAudio.play(note)
-  // }
-  // $scope.playSynth2 = function(note){
-  //   return  ngAudio.play(note)
-  // }
-  // $scope.playSynth3 = function(note){
-  //   return  ngAudio.play(note)
-  // }
-  // $scope.playSynth4 = function(note){
-  //   return  ngAudio.play(note)
-  // }
-  // $scope.playSynth5 = function(note){
-  //   return  ngAudio.play(note)
-  // }
-
-
-
-
-  //         "A":
-  //         {
-  //           "one": s.E4,
-  //           "two": s.Db4,
-  //           "three":s.A4,
-  //           "four":s.E3,
-  //           "five":s.A4
-  //         }
-  //       }
-  //     ]
-  //   }
-  // ]
-  // $log.info('?????:',$scope.tunings.Gmajor.A.three)
-
-
-
-
-
-
-
 })
